71af51e0d007b7558bfa15ea7007e7fa0e7ba7ef
SlimeNull <slimenull@outlook.com>
2021-01-27 07:05:31 +0800

    添加项目文件。

---
+++ Null.ArgsParser/ArgsParser.cs
@@ -0,0 +1,542 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Null.ArgsParser
+{
+    public enum CommandElementType
+    {
+        SwitchArgument,
+        FieldArgument,
+        PropertyArgument,
+        CommandLine,
+        Arguments,
+
+        NotArgument
+    }
+    public abstract class ArgumentElement : CommandElement
+    {
+        public abstract bool IgnoreCase { get; set; }
+        public abstract bool IsTriggered(string txt);
+        public abstract bool TryParse(ref string[] args, ref int index);
+        public abstract bool TryAssign(Type type, object instance);
+    }
+    public abstract class NamedArgumentElement : ArgumentElement
+    {
+        public abstract string Name { get; }
+        public virtual CommandElementType ArgumentType { get; } = CommandElementType.NotArgument;
+    }
+    public interface CommandElement
+    {
+        bool IgnoreCase { get; set; }
+        bool IsTriggered(string txt);
+        bool TryParse(ref string[] args, ref int index);
+        bool TryAssign(Type type, object instance);
+    }
+    public interface CommandElementContainer : ICollection<CommandElement>
+    {
+        CommandElement[] Elements { get; }
+        void Parse(string[] args);
+        T ToObject<T>();
+    }
+    public class SwitchArgument : NamedArgumentElement
+    {
+        string name = string.Empty;
+        bool ignoreCase = false;
+
+        char triggerChar = '/';
+        bool enabled = false;
+        bool parsed = false;
+
+        public override string Name { get => name; }
+        public override bool IgnoreCase { get => ignoreCase; set => ignoreCase = value; }
+        public override CommandElementType ArgumentType { get; } = CommandElementType.SwitchArgument;
+        public char TriggerChar { get => triggerChar; set => triggerChar = value; }
+
+        public bool Enabled
+        {
+            get => enabled;
+            set => enabled = value;
+        }
+
+        public SwitchArgument() { }
+        public SwitchArgument(string name)
+        {
+            this.name = name;
+        }
+        public SwitchArgument(string name, bool enabled)
+        {
+            this.name = name;
+            this.enabled = enabled;
+        }
+
+        public override bool IsTriggered(string txt)
+        {
+            if (ignoreCase)
+                return txt.ToUpper().Equals(triggerChar + name.ToUpper());
+            else
+                return txt.Equals(triggerChar + name);
+        }
+        public override bool TryParse(ref string[] args, ref int index)
+        {
+            if (IsTriggered(args[index]))
+            {
+                enabled = !enabled;
+
+                index++;
+                parsed = true;
+                return true;
+            }
+
+            return false;
+        }
+        public override bool TryAssign(Type type, object instance)
+        {
+            if (parsed)
+            {
+                FieldInfo fieldInfo = type.GetField(name);
+                if (fieldInfo != null)
+                {
+                    if (fieldInfo.FieldType == typeof(bool))
+                    {
+                        fieldInfo.SetValue(instance, enabled);
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+    public class PropertyArgument : NamedArgumentElement
+    {
+        string name = string.Empty;
+        bool ignoreCase = false;
+
+        char triggerChar = '-';
+        string value = string.Empty;
+        bool parsed = false;
+
+        public override string Name { get => name; }
+        public override bool IgnoreCase { get => ignoreCase; set => ignoreCase = value; }
+        public override CommandElementType ArgumentType { get; } = CommandElementType.SwitchArgument;
+        public char TriggerChar { get => triggerChar; set => triggerChar = value; }
+
+        public string Value
+        {
+            get => value;
+            set => this.value = value;
+        }
+
+        public PropertyArgument() { }
+        public PropertyArgument(string name)
+        {
+            this.name = name;
+        }
+        public PropertyArgument(string name, string value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+
+        public override bool IsTriggered(string txt)
+        {
+            if (ignoreCase)
+                return txt.ToUpper().Equals(triggerChar + name.ToUpper());
+            else
+                return txt.Equals(triggerChar + name);
+        }
+        public override bool TryParse(ref string[] args, ref int index)
+        {
+            if (IsTriggered(args[index]))
+            {
+                index++;
+                if (index < args.Length)
+                {
+                    value = args[index];
+
+                    index++;
+                    parsed = true;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public override bool TryAssign(Type type, object instance)
+        {
+            if (parsed)
+            {
+                FieldInfo fieldInfo = type.GetField(name);
+                if (fieldInfo != null)
+                {
+                    if (fieldInfo.FieldType == typeof(string))
+                    {
+                        fieldInfo.SetValue(instance, value);
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+    public class FieldArgument : NamedArgumentElement
+    {
+        string name = string.Empty;
+        bool ignoreCase = false;
+        CommandElementType type = CommandElementType.FieldArgument;
+
+        char triggerChar = '=';
+        string value = string.Empty;
+        bool parsed = false;
+
+        public override string Name { get => name; }
+        public override bool IgnoreCase { get => ignoreCase; set => ignoreCase = value; }
+        public override CommandElementType ArgumentType { get; } = CommandElementType.SwitchArgument;
+        public char TriggerChar { get => triggerChar; set => triggerChar = value; }
+
+        public string Value
+        {
+            get => value;
+            set => this.value = value;
+        }
+
+        public FieldArgument() { }
+        public FieldArgument(string name)
+        {
+            this.name = name;
+        }
+        public FieldArgument(string name, string value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+
+        public override bool IsTriggered(string txt)
+        {
+            if (ignoreCase)
+                return txt.ToUpper().StartsWith(name.ToUpper() + triggerChar);
+            else
+                return txt.StartsWith(name + triggerChar);
+        }
+        public override bool TryParse(ref string[] args, ref int index)
+        {
+            if (IsTriggered(args[index]))
+            {
+                value = args[index].Substring(name.Length + 1);
+
+                index++;
+                parsed = true;
+                return true;
+            }
+
+            return false;
+        }
+        public override bool TryAssign(Type type, object instance)
+        {
+            if (parsed)
+            {
+                FieldInfo fieldInfo = type.GetField(name);
+                if (fieldInfo != null)
+                {
+                    if (fieldInfo.FieldType == typeof(string))
+                    {
+                        fieldInfo.SetValue(instance, value);
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+    public class CommandLine : NamedArgumentElement, CommandElementContainer
+    {
+        string name = string.Empty;
+        bool selfIgnoreCase = false;
+        bool enabled;
+        bool parsed = false;
+
+        List<CommandElement> cmdElements = new List<CommandElement>();
+        List<string> strContent = new List<string>();
+
+        public override string Name { get => name; }
+        public bool SelfIgnoreCase { get => selfIgnoreCase; set => selfIgnoreCase = value; }
+        public CommandElement[] Elements { get => cmdElements.ToArray(); }
+        public string ExContentName { get; set; } = "ExtraContent";
+        public string[] ExtraContent { get => strContent.ToArray(); }
+
+        public int Count => cmdElements.Count;
+        public bool IsReadOnly => false;
+        public bool ElementsIgnoreCase
+        {
+            get
+            {
+                bool ignoreCase = false;
+                foreach (CommandElement element in cmdElements)
+                    ignoreCase &= element.IgnoreCase;
+                return ignoreCase;
+            }
+            set
+            {
+                foreach (CommandElement element in cmdElements)
+                    element.IgnoreCase = value;
+            }
+        }
+        public override bool IgnoreCase
+        {
+            get
+            {
+                bool ignoreCase = selfIgnoreCase;
+                foreach (CommandElement element in cmdElements)
+                    ignoreCase &= element.IgnoreCase;
+                return ignoreCase;
+            }
+            set
+            {
+                foreach (CommandElement element in cmdElements)
+                    element.IgnoreCase = value;
+                selfIgnoreCase = value;
+            }
+        }
+
+        public CommandLine() { }
+        public CommandLine(string name)
+        {
+            this.name = name;
+        }
+        public CommandLine(string name, params CommandElement[] args)
+        {
+            this.name = name;
+            cmdElements.AddRange(args);
+        }
+
+        public override bool IsTriggered(string txt)
+        {
+            if (selfIgnoreCase)
+                return txt.ToUpper().StartsWith(name.ToUpper());
+            else
+                return txt.StartsWith(name);
+        }
+        public override bool TryParse(ref string[] args, ref int index)
+        {
+            if (IsTriggered(args[index]))
+            {
+                index++;
+                enabled = true;
+
+                bool parsed;
+                for (; index < args.Length; index++)
+                {
+                    parsed = false;
+                    for (int j = 0; j < cmdElements.Count && index < args.Length; j++)
+                    {
+                        parsed |= cmdElements[j].TryParse(ref args, ref index);
+                    }
+                    if (!parsed)
+                    {
+                        strContent.Add(args[index]);
+                    }
+                }
+
+                this.parsed = true;
+                return true;
+            }
+
+            return false;
+        }
+        public override bool TryAssign(Type type, object instance)
+        {
+            if (parsed)
+            {
+                FieldInfo fieldInfo = type.GetField(name);
+                FieldInfo strCntntFieldInfo = type.GetField(ExContentName);
+                if (fieldInfo != null)
+                {
+                    if (fieldInfo.FieldType == typeof(bool))
+                    {
+                        fieldInfo.SetValue(instance, enabled);
+
+                        if (strCntntFieldInfo.FieldType.IsAssignableFrom(typeof(List<string>)))
+                            strCntntFieldInfo.SetValue(instance, strContent);
+                        else if (typeof(string[]) == strCntntFieldInfo.FieldType)
+                            strCntntFieldInfo.SetValue(instance, strContent.ToArray());
+
+                        foreach (CommandElement element in cmdElements)
+                        {
+                            element.TryAssign(type, instance);
+                        }
+
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public void Add(CommandElement argv)
+        {
+            cmdElements.Add(argv);
+        }
+        public void Clear()
+        {
+            cmdElements.Clear();
+        }
+        public bool Contains(CommandElement argv)
+        {
+            return cmdElements.Contains(argv);
+        }
+        public void CopyTo(CommandElement[] array, int index)
+        {
+            cmdElements.CopyTo(array, index);
+        }
+        bool ICollection<CommandElement>.Remove(CommandElement item)
+        {
+            return cmdElements.Remove(item);
+        }
+        public IEnumerator<CommandElement> GetEnumerator()
+        {
+            return cmdElements.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return cmdElements.GetEnumerator();
+        }
+
+
+
+
+        public void Parse(string[] args)
+        {
+            int index = 0;
+            TryParse(ref args, ref index);
+        }
+        public T ToObject<T>()
+        {
+            Type type = typeof(T);
+            T result = (T)Activator.CreateInstance(type);
+
+            TryAssign(type, result);
+            return result;
+        }
+    }
+    public class Arguments : CommandElement, CommandElementContainer
+    {
+        List<CommandElement> cmdElements = new List<CommandElement>();
+        List<string> strContent = new List<string>();
+        public CommandElement[] Elements { get => cmdElements.ToArray(); }
+
+        public int Count => cmdElements.Count;
+        public bool IsReadOnly => false;
+        public bool IgnoreCase
+        {
+            get
+            {
+                bool ignoreCase = false;
+                foreach (CommandElement element in cmdElements)
+                    ignoreCase &= element.IgnoreCase;
+                return ignoreCase;
+            }
+            set
+            {
+                foreach (CommandElement element in cmdElements)
+                    element.IgnoreCase = value;
+            }
+        }
+        public string ExContentName { get; set; } = "ExtraContent";
+        public string[] StringContent { get => strContent.ToArray(); }
+
+        public Arguments() { }
+        public Arguments(params CommandElement[] args)
+        {
+            cmdElements.AddRange(args);
+        }
+
+        public bool IsTriggered(string txt)
+        {
+            return true;
+        }
+        public bool TryParse(ref string[] args, ref int index)
+        {
+            bool parsed;
+            for (; index < args.Length; index++)
+            {
+                parsed = false;
+                for (int j = 0; j < cmdElements.Count && index < args.Length; j++)
+                {
+                    parsed |= cmdElements[j].TryParse(ref args, ref index);
+                }
+                if (!parsed)
+                {
+                    strContent.Add(args[index]);
+                }
+            }
+
+            return true;
+        }
+        public bool TryAssign(Type type, object instance)
+        {
+            FieldInfo strCntntFieldInfo = type.GetField(ExContentName);
+            if (strCntntFieldInfo != null)
+            {
+                if (strCntntFieldInfo.FieldType.IsAssignableFrom(typeof(List<string>)))
+                    strCntntFieldInfo.SetValue(instance, strContent);
+                else if (typeof(string[]) == strCntntFieldInfo.FieldType)
+                    strCntntFieldInfo.SetValue(instance, strContent.ToArray());
+            }
+
+            foreach (CommandElement element in cmdElements)
+            {
+                element.TryAssign(type, instance);
+            }
+
+            return true;
+        }
+
+        public void Add(CommandElement argv)
+        {
+            cmdElements.Add(argv);
+        }
+        public void Clear()
+        {
+            cmdElements.Clear();
+        }
+        public bool Contains(CommandElement argv)
+        {
+            return cmdElements.Contains(argv);
+        }
+        public void CopyTo(CommandElement[] array, int index)
+        {
+            cmdElements.CopyTo(array, index);
+        }
+        bool ICollection<CommandElement>.Remove(CommandElement item)
+        {
+            return cmdElements.Remove(item);
+        }
+        public IEnumerator<CommandElement> GetEnumerator()
+        {
+            return cmdElements.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return cmdElements.GetEnumerator();
+        }
+
+        public void Parse(string[] args)
+        {
+            int index = 0;
+            TryParse(ref args, ref index);
+        }
+        public T ToObject<T>()
+        {
+            Type type = typeof(T);
+            T result = (T)Activator.CreateInstance(type);
+
+            TryAssign(type, result);
+            return result;
+        }
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,228 @@
+﻿using Null.ArgsParser;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.IO;
+
+namespace TestConsole
+{
+    class Program
+    {
+        class StartupArgs
+        {
+            public bool List;
+            public bool Help;
+            public bool Create;
+            public bool Delete;
+            public string Filename;
+            public string Path;
+
+            public bool F;
+            public bool Q;
+
+            public string[] ExtraContent;
+        }
+        public static List<string> Split(string cmdline)
+        {
+            List<string> rstBulder = new List<string>();
+            StringBuilder temp = new StringBuilder();
+            bool escape = false, quote = false;
+
+            foreach (char i in cmdline)
+            {
+                if (escape)
+                {
+                    escape = false;
+                    switch (i)
+                    {
+                        case 'a':
+                            temp.Append('\a');
+                            break;
+                        case 'b':
+                            temp.Append('\b');
+                            break;
+                        case 'f':
+                            temp.Append('\f');
+                            break;
+                        case 'n':
+                            temp.Append('\n');
+                            break;
+                        case 'r':
+                            temp.Append('\r');
+                            break;
+                        case 't':
+                            temp.Append('\t');
+                            break;
+                        case 'v':
+                            temp.Append('\v');
+                            break;
+                        default:
+                            temp.Append(i);
+                            break;
+                    }
+                }
+                else
+                {
+                    if (i == '\\')
+                    {
+                        escape = true;
+                    }
+                    else
+                    {
+                        if (quote)
+                        {
+                            if (i == '"')
+                            {
+                                rstBulder.Add(temp.ToString());
+                                temp.Clear();
+                                quote = false;
+                            }
+                            else
+                            {
+                                temp.Append(i);
+                            }
+                        }
+                        else
+                        {
+                            if (i == '"')
+                            {
+                                rstBulder.Add(temp.ToString());
+                                temp.Clear();
+                                quote = true;
+                            }
+                            else if (i == ' ')
+                            {
+                                if (temp.Length > 0)
+                                {
+                                    rstBulder.Add(temp.ToString());
+                                    temp.Clear();
+                                }
+                            }
+                            else
+                            {
+                                temp.Append(i);
+                            }
+                        }
+                    }
+                }
+            }
+            if (temp.Length > 0)
+            {
+                rstBulder.Add(temp.ToString());
+            }
+
+            return rstBulder;
+        }
+        static StartupArgs Initialize()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+
+            Arguments basic = new Arguments(          // 最基础的命令行参数分析器
+                new CommandLine("Help"),                 // 包含Help指令
+                new CommandLine("List",                  // 包含List指令
+                    new FieldArgument("Path", "."))           // List指令中需要Path参数
+                        { ElementsIgnoreCase = true },        
+                new CommandLine("Create",                // 包含Create指令
+                    new FieldArgument("Filename"))            // Create指令中需要FileName参数
+                        { ElementsIgnoreCase = true},         
+                new CommandLine("Delete",                // 包含Delete指令
+                    new FieldArgument("Filename"))          // Delete指令中需要FileName参数
+                        { ElementsIgnoreCase = true}
+                )
+            { IgnoreCase = true };                      // 忽略大小写
+
+            basic.Parse(args);               // 进行分析
+
+            return basic.ToObject<StartupArgs>();       // 返回类的实例
+        }
+        static void Main()
+        {
+            StartupArgs args = Initialize();
+
+            if (args.Help)               // 表示参数中想要调用 Help 指令
+            {
+                if (args.ExtraContent.Length == 1)
+                {
+                    switch(args.ExtraContent[0].ToUpper())
+                    {
+                        case "LIST":
+                            Console.WriteLine("列举某个目录下的成员: List Path[=.]");
+                            break;
+                        case "HELP":
+                            Console.WriteLine("显示帮助手册");
+                            break;
+                        case "CREATE":
+                            Console.WriteLine("创建一个文件: Create Filename=文件名");
+                            break;
+                        case "DELETE":
+                            Console.WriteLine("删除一个文件: Delete Filename=文件名");
+                            break;
+                        default:
+                            Console.WriteLine($"未知指令: {args.ExtraContent[0]}");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("这是一个示例程序, 用于演示 Null.ArgsParser 的功能");
+                }
+            }
+            else if (args.List)                    // 表示调用 List
+            {
+                if (args.Path != null)
+                {
+                    Console.WriteLine("Files:");
+                    foreach (string i in Directory.GetFiles(args.Path))
+                        Console.WriteLine($"  {i}");
+
+                    Console.WriteLine("Directories");
+                    foreach (string i in Directory.GetDirectories(args.Path))
+                        Console.WriteLine($"  {i}");
+                }
+            }
+            else if (args.Create)        // 表示参数中想要调用 Create 指令
+            {
+                if (args.Filename != null)
+                {
+                    File.Create(args.Filename);
+                }
+                else
+                {
+                    Console.WriteLine("你必须指定一个文件名");
+                }
+            }
+            else if (args.Delete)             // 调用 Delete 
+            {
+                if (args.Filename != null)
+                {
+                    File.Delete(args.Filename);
+                }
+                else
+                {
+                    Console.WriteLine("你必须指定一个文件名");
+                }
+            }
+            else
+            {
+                Console.WriteLine("没有进行任何操作?");
+            }
+
+
+            //Arguments nargs = new Arguments(new PropertyArgument("Fuck"), new FieldArgument("Method"), new SwitchArgument("Quick"));
+            //nargs.IgnoreCase = true;
+            //nargs.Parse(args);
+            //StartupArgs qwq = nargs.ToObject<StartupArgs>();
+
+            //Console.WriteLine(
+            //    $"StartupArgs: \n" +
+            //    $"    String Fuck: {qwq.Fuck}\n" +
+            //    $"    String Method: {qwq.Method}\n" +
+            //    $"    bool Quick: {qwq.Quick}\n" +
+            //    $"    Special - ExtraContent: {qwq.ExtraContent}\n");
+#if DEBUG
+            //Console.ReadLine();
+#endif
+        }
+    }
+}
